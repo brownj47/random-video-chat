6d4ebc9ccf7caaa60adae289381cce36f055a77b
Justus B <justus.c.brown@gmail.com>
2022-08-04 12:19:36 -0700

    password hashing on create

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
+const bcrypt = require('bcrypt')
 
 class User extends Model { };
 
@@ -21,6 +22,12 @@ User.init({
     }
 }, {
     sequelize,
+    hooks:{
+        beforeCreate:userObj=>{
+            userObj.password = bcrypt.hashSync(userObj.password,4);
+            return userObj;
+        }
+    }
 }
 );
 
