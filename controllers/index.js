17cd81d3f925222587f6529caf32fb9858266114
Justus B <justus.c.brown@gmail.com>
2022-08-04 12:33:38 -0700

    password hashed on login

--- controllers/index.js
+++ controllers/index.js
@@ -1,39 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel.js')
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.render('landing')
 });
 
 // create account page
-router.get('/create-account', (req, res)=>{
+router.get('/create-account', (req, res) => {
     res.render('signup')
 });
-router.post('/create-account', (req, res)=>{
+router.post('/create-account', (req, res) => {
     console.log(req.body);
-    User.create(req.body).then(data=>{
+    User.create(req.body).then(data => {
         res.json(data)
-    }).catch(err=>{
-        res.status(500).json({msg:"ERROR",err})
+    }).catch(err => {
+        res.status(500).json({ msg: "ERROR", err })
     });
 });
 
 //login routes
-router.get('/login', (req, res)=>{
-    res.render('login')
-});
-router.post('/login', (req, res)=>{
-    console.log(req.body)
+router.get('/login', (req, res) => {
+    res.render('login');
 
-    res.json('post route connected')
+});
+router.post('/login', async (req, res) => {
+    console.log(req.body);
+    try {
+        const foundUser = await User.findOne(
+            {
+                where: {
+                    email: req.body.email
+                }
+            }
+        )
+
+        if (!foundUser) {
+            return res.status(401).json('invalid login credentials')
+        }
+
+        if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
+            return res.status(401).json('invalid login credentials')
+        }
+
+        res.json(foundUser)
+    } catch (err) {
+        console.log(err)
+    }
 });
 
 //chat routes
-router.get('/chat', (req, res)=>{
+router.get('/chat', (req, res) => {
     res.render('chat')
 });
-router.post('/chat', (req, res)=>{
+router.post('/chat', (req, res) => {
     console.log(req.body)
     res.json('post route connected')
 });
