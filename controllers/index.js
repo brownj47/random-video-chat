3bcf5dacc3b8fca55ba111a682969b39b1aa9f9b
Justus B <justus.c.brown@gmail.com>
2022-08-03 22:59:10 -0700

    basic site routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
+    res.render('landing')
+});
+router.get('/create-account', (req, res)=>{
     res.render('create')
+});
+router.get('/login', (req, res)=>{
+    res.render('login')
 });
 
 
