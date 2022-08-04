1a39156351c4b3fcafc60efa338f4df2a6387175
Justus B <justus.c.brown@gmail.com>
2022-08-04 11:24:57 -0700

    getting updates

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/userModel.js')
 
 router.get('/', (req, res)=>{
     res.render('landing')
@@ -11,6 +12,12 @@ router.get('/create-account', (req, res)=>{
 });
 router.post('/create-account', (req, res)=>{
     console.log(req.body)
+    User.create(req.body)
+    .then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        res.status(500).json({msg:"ERROR",err})
+    })
     res.json('post route connected')
 });
 
@@ -20,6 +27,7 @@ router.get('/login', (req, res)=>{
 });
 router.post('/login', (req, res)=>{
     console.log(req.body)
+
     res.json('post route connected')
 });
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,23 @@
-const { Model, DataTypes} = require('sequelize');
-
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
-
-
-class User extends Model {};
+class User extends Model { };
 
 User.init({
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            vaidate: {
-                isEmail: true
-            }
-        },
-    },{
-        sequelize
-    }
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        vaidate: {
+            isEmail: true
+        }
+    },
+}, {
+    sequelize
+}
 );
+
+module.exports = User;
