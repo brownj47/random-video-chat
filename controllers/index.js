bfc8b3075a92823f223b9e27923c1caad4295a26
Justus B <justus.c.brown@gmail.com>
2022-08-03 23:11:00 -0700

    simple get and post routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,25 @@ const router = express.Router();
 router.get('/', (req, res)=>{
     res.render('landing')
 });
+
+// create account page
 router.get('/create-account', (req, res)=>{
     res.render('create')
 });
+router.post('/create-account', (req, res)=>{
+    console.log(req.body)
+    res.json('post route connected')
+});
+
+//login routes
 router.get('/login', (req, res)=>{
     res.render('login')
 });
+router.post('/login', (req, res)=>{
+    console.log(req.body)
+    res.json('post route connected')
+});
+
 
 
 module.exports = router;
