362cda9102cc2373f48daf2f492d78206777345e
GabrielWarner <gabe0412@uw.edu>
2022-08-05 13:31:08 -0700

    update

--- public/js/videochat.js
+++ public/js/videochat.js
@@ -16,7 +16,7 @@ socket.on("chat message", function (msg) {
   const item = document.createElement("li");
   item.textContent = msg;
   messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  window.scrollTo(0, document.getElementById('messages').offsetHeight);
 });
 
 //creates new peer object giving current host and setting id to undefined.
