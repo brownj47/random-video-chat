0415bfbdb50500806105dfc5f687cb05ae3602ed
Justus B <justus.c.brown@gmail.com>
2022-08-04 19:17:58 -0700

    session details set on account creation

--- controllers/createAccountRoutes.js
+++ controllers/createAccountRoutes.js
@@ -7,10 +7,18 @@ const bcrypt = require('bcrypt')
 router.get('/', (req, res) => {
     res.render('signup')
 });
+
 router.post('/', (req, res) => {
     console.log(req.body);
     User.create(req.body).then(data => {
         res.json(data)
+
+        req.session.user = {
+            id: data.id,
+            username: data.username,
+            email: data.email
+        }
+
     }).catch(err => {
         res.status(500).json({ msg: "ERROR", err })
     });
